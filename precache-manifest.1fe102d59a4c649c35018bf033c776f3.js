d21baee46ef763b081b480382a0e86ba90c7ae12
Eldar Celik <eldar.celik21@gmail.com>
2020-09-17 19:13:46 +0200

    Updates

---
+++ precache-manifest.1fe102d59a4c649c35018bf033c776f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56c79e2b04eace03b4014a376695c32",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "d4202711328a74cc9f7a",
+    "url": "/weather-react-app/static/css/main.3e54c18f.chunk.css"
+  },
+  {
+    "revision": "20f682600f3427846bf2",
+    "url": "/weather-react-app/static/js/2.b4c69cdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react-app/static/js/2.b4c69cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4202711328a74cc9f7a",
+    "url": "/weather-react-app/static/js/main.4c47eb83.chunk.js"
+  },
+  {
+    "revision": "80a11080e1d5d2b66f46",
+    "url": "/weather-react-app/static/js/runtime-main.42a25d67.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather-react-app/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather-react-app/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
